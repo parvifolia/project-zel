539fedb6426adca477aba2161bab57f3b4536ccd
parvifolia <parvifoli@gmail.com>
2020-05-19 22:59:35 +0300

    v2.02 : Cv page fixes

--- static/portfolio/js/app.js
+++ static/portfolio/js/app.js
@@ -1,11 +1,38 @@
+// Query Selectors
 const cvItemWrap = document.querySelectorAll('.cv-item-wrap');
 const cvItemText = document.querySelector('.cv-item-text');
 
+// Tab Expanding and Text Appearing
 cvItemWrap.forEach( item => {
     item.addEventListener('click', e =>{
-        e.target.classList.toggle('cv-items-open');
-        //console.log(e.target.nextElementSibling.children[0].classList.toggle('cv-items-text-open'));
-        e.target.children[1].children[0].classList.toggle('cv-items-text-open');
+        if (e.target.classList.contains('cv-item-wrap-1')){
+            console.log(e.target)
+            e.target.children[1].children[0].classList.toggle('cv-items-text-open-1');
+        }
+        else if (e.target.classList.contains('cv-item-wrap-2')){
+            e.target.children[1].children[0].classList.toggle('cv-items-text-open-2');
+        }
+        else if (e.target.classList.contains('cv-item-wrap-3')){
+            e.target.children[1].children[0].classList.toggle('cv-items-text-open-3');
+        }
+        else if (e.target.classList.contains('cv-item-wrap-4')){
+            e.target.children[1].children[0].classList.toggle('cv-items-text-open-4');
+        }
+        else if (e.target.classList.contains('cv-item-wrap-5')){
+            e.target.children[1].children[0].classList.toggle('cv-items-text-open-5');
+        }
+        else if (e.target.classList.contains('cv-item-wrap-6')){
+            e.target.children[1].children[0].classList.toggle('cv-items-text-open-6');
+        }
+        else if (e.target.classList.contains('cv-item-wrap-7')){
+            e.target.children[1].children[0].classList.toggle('cv-items-text-open-7');
+        }
+        else if (e.target.classList.contains('cv-item-wrap-8')){
+            e.target.children[1].children[0].classList.toggle('cv-items-text-open-8');
+        }
+        else if (e.target.classList.contains('cv-item-wrap-9')){
+            e.target.children[1].children[0].classList.toggle('cv-items-text-open-9');
+        }
     })
 })
 
